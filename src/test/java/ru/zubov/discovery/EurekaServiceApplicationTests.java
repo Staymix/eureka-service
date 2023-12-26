a471e4e74c6c00c9156d5dd82f74e7258504d735
Staymix <staszubov199530@gmail.com>
2023-12-26 13:12:22 +0600

    Add project

---
+++ src/test/java/ru/zubov/discovery/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zubov.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
